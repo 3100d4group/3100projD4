f55db358fd14a8ce41831150439656dc38c38a4c
elvislau0205 <75233300+elvislau0205@users.noreply.github.com>
2022-04-04 12:05:26 +0800

    send email

--- createAccount.js
+++ createAccount.js
@@ -14,10 +14,32 @@ app.get('/',function(req,res){
 app.listen(3000);
 
 app.post('/process', function(req, res) {
-    console.log('Form : ' + req.query.form);
-    console.log('Name : ' + req.body.name);
+    console.log('Name : ' + req.body.username);
     console.log('Email : ' + req.body.email);
     console.log('Password : ' + req.body.password);
+
+    var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'csci3100proj4@gmail.com',
+        pass: '@b123456'
+      }
+    });
+    
+    var mailOptions = {
+      from: 'csci3100proj4@gmail.com',
+      to: req.body.email,
+      subject: 'Email verification',
+      text: 'Testing'
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+
     res.sendFile(path.join(__dirname+'/login.html'));
   });
-
