994b46d4ed6d80898045d1d943f970ad2d3d0cc6
yhcheng0 <80042796+yhcheng0@users.noreply.github.com>
2022-04-08 19:47:24 +0800

    Create buyproduct.js

---
+++ buyproduct.js
@@ -0,0 +1,2 @@
+//To do: need to add  to buyproduct.js :
+//<div class="list-group-item list-group-item-action"><span>(product)hello </span><span style="float: right">(price)$100 * (quantity)10</span></div>
