786dda7afccc078b07e8dcf097727780dfb80aa4
WongNgouShan <79697387+WongNgouShan@users.noreply.github.com>
2022-04-09 18:20:01 +0800

    Update changepassword.js

--- changepassword.js
+++ changepassword.js
@@ -1,22 +1,5 @@
 //this is changepassword.js
-/*var http = require('http');
-var fs = require('fs');*/
-
-
-var express = require('express');
-var app = express();
-/*var path = require("path");
-var mongo = require('mongodb');
-var MongoClient = require('mongodb').MongoClient;
-var url ="mongodb+srv://CSCI3100:Ab123456@cluster0.wkhhe.mongodb.net/User?retryWrites=true&w=majority";
-*/
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://stu128:p090058-@csci2720.m2qbq.mongodb.net/stu128');
-
 const schema = require("./Schemas.js");
-
-app.use(require('body-parser')());
 
 
 exports.changepassword =  function(req, res) {
@@ -25,8 +8,11 @@ exports.changepassword =  function(req, res) {
 
         if(req.body.current == user.passWord && req.body.new==req.body.confirm){
             user.passWord = req.body.new;
+            user.save();
+            res.send("Password updated!");
         }
-        user.save();
+        else{
+            res.send("Wrong old password or confirm password not match")
+        }
     });
   };
-//Todo:how to link to form: /changepwd
