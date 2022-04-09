911ab0749ac0e3d54ba4ac49dfb51a89ef4c005e
pmng9 <80062906+pmng9@users.noreply.github.com>
2022-04-10 05:23:49 +0800

    Update handleproduct.js

--- handleproduct.js
+++ handleproduct.js
@@ -7,7 +7,17 @@ const schema = require("./Schemas.js");
 
 exports.handleProd = function(req,res){
     if (req.body.action == 'Buy'){
-        res.send("ToDo: buy");
+        let conditions={productId: req.body.product},update={$inc:{remain:-1}}
+        schema.Product.updateOne(conditions, update, (err,e)=>{
+            if (err){
+                res.send(err);
+            }
+            
+            else{
+                //TODO: add to purchase history list
+                res.render(path.join(__dirname + '/purchasehistory.html'),{message:"<h1>Purchase Success</h1><p>Please check your purchase record and contact the seller.</p>"})
+            }
+        });
     }
     else if(req.body.action == 'Delete'){
         schema.Product.findOne({ productId: req.body.product}).remove((err,e)=>{
