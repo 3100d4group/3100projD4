0f611577f4d62f51878a837a158e48a1b8b0e658
WongNgouShan <79697387+WongNgouShan@users.noreply.github.com>
2022-04-10 03:59:32 +0800

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -5,11 +5,17 @@ const express = require('express'),
 const schema = require("./Schemas.js");
 
 exports.loadHome = function(req,res){
-    schema.Product.find().populate('seller').exec(function(err, products){    
+    schema.User.findOne( {email: req.session.user}) .exec(function(err, user){    
         if (err) 
             res.send(err); 
         else{
-            res.render(__dirname+'/homepage.ejs',{products: products});
+            schema.Product.find().populate('seller').exec(function(err, products){    
+                if (err) 
+                    res.send(err); 
+                else{
+                    res.render(__dirname+'/homepage.ejs',{products: products, user:user});
+                    }
+            });     
             }
-    });  
+    });   
 };
