9bd5312b541be9c768e85279b2b8c7617e4bcba6
WongNgouShan <79697387+WongNgouShan@users.noreply.github.com>
2022-04-10 03:53:16 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,56 +1,49 @@
-const fs = require('fs');
-
-const path = require("path");
-
+//this is admin.js
 const schema = require("./Schemas.js");
 
-const express = require('express');
-const app = express();
-app.use(require('body-parser')());
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://stu128:p090058-@csci2720.m2qbq.mongodb.net/stu128');
-
-exports.displayUsers = function(req,res){
-    fs.readFile(__dirname+'/admin.html', (err, html)=> {
-        if (err) {
+exports.displayUsers = display;
+function display(req,res,message){
+    schema.User.find().exec(function(err, users){
+        if (err){
             res.send(err);
         }
         else{
-            res.write(html);
-            schema.User.find().populate('userID').exec(function(err, f){
-                if (err) {
-                    res.send(err);
-                }
-
-            else{
-                f.forEach(function(e){
-
-            res.write("\
-                        <div class=\"card col-6 col-md-4 col-xl-3 m-3\" style=\"height : 20rem;\">\
-                        <img src=\"\" class=\"card-img-top\" alt=\"...\">\
-                        <div class=\"card-body\">\
-                        <h5 class=\"card-title\">User ID: "+e.userId+"<br></h5>\
-                        <p class=\"card-text\">Password: "+e.passWord+"<br>\
-                        Email: "+e.email+"</p>\
-                        <a href=\"#\" class=\"btn btn-primary\">Reset Password</a>\
-                        <a href=\"#\" class=\"btn btn-primary\">Delete Account</a>\
-                        </div>\
-                        </div>");
-        });
-    }
-})
-}})}
+            res.render(__dirname+'/admin.ejs',{users: users,message:message});
+        }
+    });
+}
 
 exports.resetPW=function(req,res){
-    let conditions={userId: req.body.usertochange}, update={$set:{passWord: req.body.passwordreset}};
-    document.User.updateOne(conditions, update, (err,e)=>{
-        if (err){
-            res.send(err);
-          }
-          else if(e != null){
-            res.send("Reset pasword success.");
-          }
-    });
-
+    if (req.body.action == 'Reset'){
+        let conditions={email: req.body.userToChange}, update={$set:{passWord: req.body.passwordReset}};
+        schema.User.updateOne(conditions, update, (err,e)=>{
+            if (err){
+                res.send(err);
+            }
+            else if(e.modifiedCount == 0){
+                display(req,res,"<p style=\"color:red\" class=\"container\">Error: none updated.</p>");
+            }
+            else{
+                display(req,res,"<p style=\"color:green\" class=\"container\">Password of "+ req.body.userToChange +" has been updatted.</p>");
+            }
+        });
+    }
+    else if(req.body.action == 'Delete'){
+        let foundUserable = schema.User.findOne({ email: req.body.userToChange, isAdmin: false });
+        foundUserable.remove((err,e)=>{
+            if (err){
+                res.send(err);
+            }
+            else if(e.deletedCount == 0){
+                display(req,res,"<p style=\"color:red\" class=\"container\"> Error: none deleted. Note: You cannot delete an admin.</p>");
+            }
+            else{
+                console.log(e);
+                display(req,res,"<p style=\"color:green\" class=\"container\"> Delete successful.</p>");
+            }
+        });
+    }
+    else
+    display(req,res,"<p style=\"color:red\" class=\"container\"> Error: unknown action.</p>");
 }
