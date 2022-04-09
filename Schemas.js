602ac5edb46cf77f979a6a160ebc502cececba64
WongNgouShan <79697387+WongNgouShan@users.noreply.github.com>
2022-04-10 03:52:15 +0800

    Update Schemas.js

--- Schemas.js
+++ Schemas.js
@@ -5,19 +5,20 @@ const ProductSchema = mongoose.Schema({
     productId: { type: Number, required: true,
     unique: true },
     name: { type: String, required: true },
-    price: { type: Number },
+    price: { type: Number, required: true },
     seller: {type: mongoose.ObjectId, ref: 'User'},
-    remain: {type: Number},
+    remain: {type: Number, required: true},
     picture:{ data: Buffer, contentType: String },
-    contact:{type: String}
+    contact:{type: String, required: true}
     });
 const UserSchema = mongoose.Schema({
     userId: { type: String, required: true,
     unique: true },
     passWord:{type: String, required: true },
-    email:{type: String, required: true},
+    email:{type: String, required: true, unique: true},
     picture:{ data: Buffer, contentType: String },
-    verify:{type: Boolean, required: true}
+    verify:{type: Boolean, required: true},
+    isAdmin:{type: Boolean, required: true}
     });
 const Producta = mongoose.model('Product',ProductSchema);
 const Usera = mongoose.model('User',UserSchema);
