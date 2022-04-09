ced7f82dbd16fba952977e0d0916aad2b1968289
WongNgouShan <79697387+WongNgouShan@users.noreply.github.com>
2022-04-09 17:15:46 +0800

    Update createproduct.js

--- createproduct.js
+++ createproduct.js
@@ -1,25 +1,10 @@
 //this is createproduct.js
-/*var http = require('http');
-var fs = require('fs');*/
-var express = require('express');
-var app = express();
-/*var path = require("path");
-var mongo = require('mongodb');
-var MongoClient = require('mongodb').MongoClient;
-var url ="mongodb+srv://CSCI3100:Ab123456@cluster0.wkhhe.mongodb.net/User?retryWrites=true&w=majority";
-*/
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://stu128:p090058-@csci2720.m2qbq.mongodb.net/stu128');
+const fs = require('fs'),
+  express = require('express'),
+  app = express(),
+  path = require("path");
 
 const schema = require("./Schemas.js");
-
-app.use(require('body-parser')());
-
-/*app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/createproduct.html'));
-  });*/
-  
 
 exports.createProduct =  function(req, res) {
   schema.Product.findOne()
@@ -40,7 +25,6 @@ exports.createProduct =  function(req, res) {
               newId = 0;
             else
               newId = e.productId+1;
-            console.log(newId);
             postUserId = f._id;
             schema.Product.create({
               productId: newId,
@@ -48,6 +32,10 @@ exports.createProduct =  function(req, res) {
               remain: req.body.quantity,
               price: req.body.price,
               seller: f._id,
+              picture: {
+                data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+                contentType: req.file.mimetype
+              },
               contact: req.body.contact
             },(err,g)=>{
               if (err) 
@@ -60,15 +48,4 @@ exports.createProduct =  function(req, res) {
         });
       }
     });
-    /*MongoClient.connect(url, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("Product");
-      var myobj = { productname: req.body.productname, quantity: req.body.quantity, remain: req.body.quantity, price: req.body.price, contact: req.body.contact ,picture:req.body.productpic};
-      dbo.collection("Users").insertOne(myobj, function(err, res) {
-        if (err) throw err;
-        console.log("1 document inserted");
-        db.close();
-      });
-    });*/
-
   };
