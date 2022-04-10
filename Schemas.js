2ce2d330d75c9ade91ed892457f0d4f446749c5a
WongNgouShan <79697387+WongNgouShan@users.noreply.github.com>
2022-04-11 01:17:21 +0800

    Update Schemas.js

--- Schemas.js
+++ Schemas.js
@@ -6,11 +6,11 @@ const ProductSchema = mongoose.Schema({
     unique: true },
     name: { type: String, required: true },
     price: { type: Number, required: true },
-    seller: {type: mongoose.ObjectId, ref: 'User'},
+    seller: {type: mongoose.ObjectId, ref: 'User', required: true},
     remain: {type: Number, required: true},
     picture:{ data: Buffer, contentType: String },
     contact:{type: String, required: true}
-    });
+});
 const UserSchema = mongoose.Schema({
     userId: { type: String, required: true,
     unique: true },
@@ -18,11 +18,18 @@ const UserSchema = mongoose.Schema({
     email:{type: String, required: true, unique: true},
     picture:{ data: Buffer, contentType: String },
     verify:{type: Boolean, required: true},
-    isAdmin:{type: Boolean, required: true},
-    purchaseditem: {type: Array}
-    });
+    isAdmin:{type: Boolean, required: true}    
+});
+const HistorySchema = mongoose.Schema({
+    buyer:{type: mongoose.ObjectId, ref: 'User', required: true},
+    product:{type: mongoose.ObjectId, ref: 'Product', required: true},
+    quantity: { type: Number, required: true },
+    date: {type: Date ,required:true}
+});    
 const Producta = mongoose.model('Product',ProductSchema);
 const Usera = mongoose.model('User',UserSchema);
+const Historya = mongoose.model('History',HistorySchema);
 
 exports.User = Usera;
 exports.Product = Producta;
+exports.History = Historya;
