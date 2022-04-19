e9dd7b982d06360e8cc7c69bac66a3fb71894545
elvislau0205 <75233300+elvislau0205@users.noreply.github.com>
2022-04-19 10:18:57 +0800

    Update createAccount.js

--- createAccount.js
+++ createAccount.js
@@ -17,15 +17,15 @@ app.use(session({
 //module functions
 exports.userLogin = function(req,res){
   let conditions = {email: req.body.email, passWord: req.body.password, verify: true};
-  doc.User.findOne(conditions,(err,e)=>{
+  doc.User.findOne(conditions,(err,e)=>{//find inputted email and password
     if (err){
       res.send(err);
     }
-    else if(e != null){
+    else if(e != null){//if such record exist
       req.session.user = req.body.email; //requires js session
       res.redirect('/home');
     }
-    else{
+    else{////if such record don't exist
       res.render(__dirname+'/login.ejs',{message:"<p style=\"color:red\">Sorry, but it seems that the account does not exist or you have enter an incorrect password. Please try again.</p>"});
     }
   });
@@ -33,17 +33,17 @@ exports.userLogin = function(req,res){
 
 exports.userVerify = function(req,res){
     var q = url.parse(req.url, true);
-    var qdata = q.query;
+    var qdata = q.query;//get user data from url link in email sent
 
     let conditions = {userId: qdata.username,verify:false}, update = {$set:{verify: true}};
-    doc.User.updateOne(conditions, update,(err,e)=>{
+    doc.User.updateOne(conditions, update,(err,e)=>{//verify specified user as above
         if (err){
           res.send(err);
         }
-        else if(e.modifiedCount != 0){
+        else if(e.modifiedCount != 0){//if user is verified
           res.render(path.join(__dirname + '/toLogin.ejs'),{message:"<h1>You are good to go!</h1><p>Login to start shopping!</p>"});
         }
-        else{
+        else{//if user is not verified
           res.render(path.join(__dirname + '/toLogin.ejs'),{message:"<h1>Opps...</h1><p>Seems like there are some errors, or your have already been verified.</p>"});
       }
 
@@ -53,7 +53,7 @@ exports.userVerify = function(req,res){
 
 exports.registerAccount = function (req, res) {
 
-    var transporter = nodemailer.createTransport({
+    var transporter = nodemailer.createTransport({//log in our gmail
       service: 'gmail',
       auth: {
         user: 'csci3100proj4@gmail.com',
@@ -61,14 +61,14 @@ exports.registerAccount = function (req, res) {
       }
     });
     
-    var mailOptions = {
+    var mailOptions = {//create verification email
       from: 'csci3100proj4@gmail.com',
       to: req.body.email,
       subject: 'Email verification',
       html: '<a href="ec2-3-94-179-234.compute-1.amazonaws.com:3000/verify?username=' + req.body.username + '">click here to verify your account</a>'
     };
 
-    transporter.sendMail(mailOptions, function(error, info){
+    transporter.sendMail(mailOptions, function(error, info){//Sent email from our email to register email
       if (error) {
         console.log(error);
       } else {
